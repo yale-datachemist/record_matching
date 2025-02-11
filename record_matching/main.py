4b34c06f5a55db40688eae0ef00ae71348d21af3
Tim Thompson <timathom@gmail.com>
2025-02-11 17:41:13 -0500

    Change key.

--- record_matching/main.py
+++ record_matching/main.py
@@ -46,7 +46,8 @@ TEMPLATES = {
     "subjects": "{{#if subjects}}Subjects: {{subjects}}\n{{/if}}",
     "genres": "{{#if genres}}Genres: {{genres}}\n{{/if}}",
     "relatedwork": "{{#if relatedwork}}Related work: {{relatedwork}}\n{{/if}}",
-    "nametitle": "{{person}}\n{{title}}",
+    "nametitle": "{{person}}\n{{title}}\n",
+    "personroles": "Person: {{person}}\nRoles: {{roles}}\n"
 }
 
 TEMPLATES["composite"] = (
@@ -142,7 +143,7 @@ def average_fields():
 def build_index_map():
     ctx = build_session_context()
 
-    ctx.table("templated").filter(df.col("key") == "nametitle").sort(
+    ctx.table("templated").filter(df.col("key") == "personroles").sort(
         df.col("hash")
     ).select(
         (df.functions.row_number() - 1).alias("vector_id"),
@@ -256,7 +257,7 @@ def discover_training_set():
 def get_record_from_vid(ctx, vid) -> Dict:
     templated_df = (
         ctx.table("templated")
-        .filter(df.col("key") == "nametitle")
+        .filter(df.col("key") == "personroles")
         .select(df.col("templated"), df.col("id").alias("tid"))
     )
     result = (
@@ -292,10 +293,10 @@ def ask_oracle_with_vid(ctx, vid1, vid2):
 
 def ask_oracle_with_id(ctx, id1, id2):
     record1 = ctx.sql(
-        f"SELECT templated FROM templated WHERE key = 'nametitle' AND id = {id1}"
+        f"SELECT templated FROM templated WHERE key = 'personroles' AND id = {id1}"
     ).to_pylist()[0]
     record2 = ctx.sql(
-        f"SELECT templated FROM templated WHERE key = 'nametitle' AND id = {id2}"
+        f"SELECT templated FROM templated WHERE key = 'personroles' AND id = {id2}"
     ).to_pylist()[0]
     return ask_oracle(record1["templated"], record2["templated"])
 
@@ -512,6 +513,7 @@ def search_string(query: str, ann: Optional[ANN] = None) -> df.DataFrame:
             df.col("subjects"),
             df.col("title"),
             df.col("nametitle"),
+            df.col("personroles"),
         )
     )
 
